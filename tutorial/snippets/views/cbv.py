e82ed4c34fccbe35ce30159b5929a1c432c848f3
janggunhee <jan9.9unhee@gmail.com>
2017-11-06 22:38:59 +0900

    add ListAPIView,RetrieveAPIView

--- tutorial/snippets/views/cbv.py
+++ tutorial/snippets/views/cbv.py
@@ -1,27 +1,36 @@
+from django.contrib.auth.models import User
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import permissions
 
 from ..models import Snippet
-from ..serializers import SnippetSerializer
+from ..serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    def post(self,request, format=None):
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
     def get_object(self, pk):
         try:
             return Snippet.objects.get(pk=pk)
@@ -45,3 +54,14 @@ class SnippetDetail(APIView):
         snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
