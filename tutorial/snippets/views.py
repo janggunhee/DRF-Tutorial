6355b6545377666a1e8137a6960e8e4c34f6e4d2
janggunhee <jan9.9unhee@gmail.com>
2017-11-06 15:28:53 +0900

    @api_view형태로 동작하며 Response, Request객체를사용하도록 리팩토링

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from .models import Snippet
 from snippets.serializers import SnippetSerializer
@@ -16,7 +19,7 @@ config/urls.py에 snippets.urls를 include
     /snippets/에 연결되도록 url을 구성 
 """
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def snippet_list(request):
     if request.method == 'GET':
         # snippets는 모든 Snippet의 쿼리셋
@@ -26,47 +29,47 @@ def snippet_list(request):
         # JSON방식으로 response
         # 기본적으로 JsonResponse는 dict형 객체를 받아 처리하나,
         # safe옵션이 False
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         # request로 전달된 데이터들을 JSONParser를 사용해 파이썬 데이터 형식으로 파싱
-        data = JSONParser().parse(request)
+        #data = JSONParser().parse(request)
         # 처리된 데이터를 사용해 SnippetSerializer인스턴스를 생성
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         # 인스턴스에 주어진 데이터가 유효할 경우
         if serializer.is_valid():
             # 인스턴스의 save()메서드를 호출해 Snippet객체를 생성
             serializer.save()
             # HTTP상태코드(201 created)로 Snippet생성에 사용된 serializer의 내용을 보내줌
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         # 유효하지 않으면 인스턴스의 에러들을 HTTP 400 Bad request상태코드와 함께 보내줌
-        return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', "DELETE"])
 def snippet_detail(request, pk):
     # pk에 해당하는 Snippet이 존재하는지 확인 후 snippet변수에 할당
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         # GET 요청시에는 snippet을 serializer한 결과를 보여줌
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         # PUT 요청시에는 전달된 데이터를 이용해서 snippet인스턴스의 내용을 변경
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        # data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         # DELETE요청시에는 해당 Snippet인스턴스를 삭제
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
