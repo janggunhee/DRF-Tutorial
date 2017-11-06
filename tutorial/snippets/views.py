7f7af8452e3e4691381174410dce55f5d4ad6414
janggunhee <jan9.9unhee@gmail.com>
2017-11-06 15:15:29 +0900

    snippet_detail view주석 추가

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -45,19 +45,19 @@ def snippet_list(request):
 
 @csrf_exempt
 def snippet_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
+    # pk에 해당하는 Snippet이 존재하는지 확인 후 snippet변수에 할당
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
         return HttpResponse(status=404)
 
     if request.method == 'GET':
+        # GET 요청시에는 snippet을 serializer한 결과를 보여줌
         serializer = SnippetSerializer(snippet)
         return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
+        # PUT 요청시에는 전달된 데이터를 이용해서 snippet인스턴스의 내용을 변경
         data = JSONParser().parse(request)
         serializer = SnippetSerializer(snippet, data=data)
         if serializer.is_valid():
@@ -66,6 +66,7 @@ def snippet_detail(request, pk):
         return JsonResponse(serializer.errors, status=400)
 
     elif request.method == 'DELETE':
+        # DELETE요청시에는 해당 Snippet인스턴스를 삭제
         snippet.delete()
         return HttpResponse(status=204)
 
