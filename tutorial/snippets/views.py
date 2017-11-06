964b8addcea681f5239eec50da260a6434800abd
janggunhee <jan9.9unhee@gmail.com>
2017-11-06 14:07:24 +0900

    snippet_list view및 url구현

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,33 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from .models import Snippet
+from snippets.serializers import SnippetSerializer
+
+"""
+snippets/urls.py에 urlpatterns작성
+config/urls.py에 snippets.urls를 include
+
+아래의 snippet_list 뷰가
+    /snippets/에 연결되도록 url을 구성 
+"""
+
+@csrf_exempt
+def snippet_list(request):
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
