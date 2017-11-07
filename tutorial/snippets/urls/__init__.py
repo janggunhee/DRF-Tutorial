1fb8a13811aca2080db8673e82ce8b816f8da47d
janggunhee <jan9.9unhee@gmail.com>
2017-11-07 14:40:44 +0900

    tutorial4

--- tutorial/snippets/urls/__init__.py
+++ tutorial/snippets/urls/__init__.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^fbv/', include(fbv, namespace='fbv')),
     url(r'^cbv/', include(cbv, namespace='cbv')),
     url(r'^cbv-mixins/', include(cbv_mixins, namespace='cbv_mixins')),
-    url(r'^cbv-generics/', include(cbv_generics, namespace='cbv_mixins')),
+    url(r'^cbv-generics/', include(cbv_generics, namespace='cbv_generics')),
 ]
