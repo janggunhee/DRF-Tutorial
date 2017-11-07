1c24e2cddc9ce40a5af0dd31758b58745885de83
janggunhee <jan9.9unhee@gmail.com>
2017-11-07 12:21:41 +0900

    SnippetList의 perform_create()메서드 오버라이드

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -18,7 +18,7 @@ class Snippet(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='monokai', max_length=100)
     owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
-    highlighted = models.TextField()
+    highlighted = models.TextField(blank=True)
 
     class Meta:
         ordering = ('created',)
